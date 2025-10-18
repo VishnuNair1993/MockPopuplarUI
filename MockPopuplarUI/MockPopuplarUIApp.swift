46f6761736eb4d70e2e38b79051a0938f91edb5e
Vishnu Nair <vishnunair2011@gmail.com>
2025-10-18 09:07:24 +0530

    Initial Commit

---
+++ MockPopuplarUI/MockPopuplarUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MockPopuplarUIApp.swift
+//  MockPopuplarUI
+//
+//  Created by Vishnu M Nair on 18/10/25.
+//
+
+import SwiftUI
+
+@main
+struct MockPopuplarUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
